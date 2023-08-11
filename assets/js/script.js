a2fe781db7ef8a8cf81360f790f21dfe490d5ffe
fdasib <21faizal.farizi@poltekindonusa.ac.id>
2023-08-11 09:59:47 +0700

    awal

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+// Memberi nomor
+let nomor = document.querySelectorAll(".nomor");
+nomor.forEach((e, i) => {
+    e.textContent = i + 1;
+});
+
+// Cari
+let cari = document.getElementById("cari");
+let barang = document.querySelectorAll("#barang tr td");
+cari.addEventListener("input", function() {
+    // console.log(barang[0].parentNode.parentNode.textContent.toLowerCase().indexOf(cari.value.toLowerCase()) < 0);
+    barang.forEach(e => {
+        if(e.parentNode.textContent.toLowerCase().indexOf(cari.value.toLowerCase()) > -1){
+            document.querySelector("#tidak-ditemukan").parentNode.style.display = "none";
+            e.parentNode.style.display = "";
+        } else {
+            e.parentNode.style.display = "none";
+        }
+    });
+    if (barang[0].parentNode.parentNode.textContent.toLowerCase().indexOf(cari.value.toLowerCase()) < 0) {
+        console.log("ok");
+        document.querySelector("#tidak-ditemukan").parentNode.style.display = "";
+    }
+});
